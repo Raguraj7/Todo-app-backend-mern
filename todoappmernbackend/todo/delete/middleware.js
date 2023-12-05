04986e8882b21c37797d4012af86396c6ac51a29
Raguraj@2000 <101414099+Raguraj7@users.noreply.github.com>
2023-12-05 07:53:01 +0000

    commit

--- todoappmernbackend/todo/delete/middleware.js
+++ todoappmernbackend/todo/delete/middleware.js
@@ -3,16 +3,46 @@ import { newDbCluster } from '../../database/DBManager.js';
 
 const Db = await newDbCluster();
 
+// export const deleteOneTODO = async (req, res) => {
+//   const { _id } = req.query;
+//   console.log('query', req.query);
+//   const deletedata = await Db
+//     .collection('Todo-list')
+//     .deleteOne({ _id: new ObjectId(_id) });
+//   console.log('deletedata', deletedata);
+//   res.status(204).json({
+//     _id,
+//     message: 'delete successfully',
+//   });
+// };
+  
 export const deleteOneTODO = async (req, res) => {
   const { _id } = req.query;
-  console.log('query', req.query);
+  // const deletedata = await Db
+  //   .collection('Todo-list')
+  //   .deleteOne({ _id: new ObjectId(_id) });
   const deletedata = await Db
     .collection('Todo-list')
-    .deleteOne({ _id: new ObjectId(_id) });
-  console.log('deletedata', deletedata);
-  res.status(204).json({
-    _id,
-    message: 'delete successfully',
-  });
-};
+    .find({ _id: new ObjectId(_id) });
+res.status(204).json({
+      _id,
+      message: 'delete successfully',deletedata
+    });
   
+  //  await Db.collection('todo-completed').insertOne({
+  //   username,
+  //   taskname,
+  //   description,
+  //   categeory,
+  //   date
+  // });
+  //   const deletedata = await Db
+  //     .collection('Todo-list')
+  //     .deleteOne({ _id: new ObjectId(_id) });
+  //   console.log('deletedata', deletedata);
+  //   res.status(200).json({
+  //     _id ,
+  //     message: 'delete successfully',
+  //   });
+
+};
